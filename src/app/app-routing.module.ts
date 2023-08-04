b44e1fa8b1c543a5f9a109406fe8b23e965b7fac
phucloi11 <phucloi111279@gmail.com>
2023-08-04 22:07:34 +0700

    first commit

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,10 +1,13 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { DataViewComponent } from './share/data-view/data-view.component';
 import { PickListComponent } from './share/pick-list/pick-list.component';
 import { TableComponent } from './share/table/table.component';
 import { GalleriaComponent } from './share/galleria/galleria.component';
 
 const routes: Routes = [
+  {path: 'dataview' , component: DataViewComponent}
+
   { path: 'galleria', component: GalleriaComponent },
   { path: 'picklist', component: PickListComponent },
   { path: 'table', component: TableComponent },
--- src/app/share/share.module.ts
+++ src/app/share/share.module.ts
@@ -1,8 +1,14 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { DataViewComponent } from './data-view/data-view.component';
 import { PickListComponent } from './pick-list/pick-list.component';
 
-import { PickListModule } from 'primeng/picklist';
+import { FormsModule } from '@angular/forms';
+import { TableModule } from 'primeng/table';
+import { DataViewModule, DataViewLayoutOptions } from 'primeng/dataview';
+import { RatingModule } from 'primeng/rating';
+import { TagModule } from 'primeng/tag';
+import { ButtonModule } from 'primeng/button';import { PickListModule } from 'primeng/picklist';
 import { GalleriaModule } from 'primeng/galleria';
 import { TableModule } from 'primeng/table';
 import { GalleriaComponent } from './galleria/galleria.component';
@@ -10,12 +16,24 @@ import { TableComponent } from './table/table.component';
 
 @NgModule({
   declarations: [
+    DataViewComponent
+
+
     PickListComponent,
     GalleriaComponent,
     TableComponent
   ],
   imports: [
     CommonModule,
+    TableModule,
+    DataViewModule,
+    RatingModule,
+    TagModule,
+    FormsModule,
+    ButtonModule
+  ],
+  exports:[
+    DataViewComponent,
     PickListModule,
     GalleriaModule,
     TableModule,
