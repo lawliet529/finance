fb10ae534ba1036ff2b785bb01dc9b962817ece2
TuanMinh0811 <tuanminhvo2002@gmail.com>
2023-08-05 10:40:18 +0700

    Update component orderlist

--- src/app/share/share.module.ts
+++ src/app/share/share.module.ts
@@ -5,21 +5,27 @@ import { PickListComponent } from './pick-list/pick-list.component';
 import { PickListModule } from 'primeng/picklist';
 import { GalleriaModule } from 'primeng/galleria';
 import { TableModule } from 'primeng/table';
+import { OrderListModule } from 'primeng/orderlist'
+import { TagModule } from 'primeng/tag';
 import { GalleriaComponent } from './galleria/galleria.component';
 import { TableComponent } from './table/table.component';
+import { OrderListComponent } from './order-list/order-list.component';
 
 @NgModule({
   declarations: [
     PickListComponent,
     GalleriaComponent,
-    TableComponent
+    TableComponent,
+    OrderListComponent,
   ],
   imports: [
     CommonModule,
     PickListModule,
     GalleriaModule,
     TableModule,
+    OrderListModule,
+    TagModule
   ],
-  exports: [PickListComponent, GalleriaComponent, TableComponent],
+  exports: [PickListComponent, GalleriaComponent, TableComponent, OrderListComponent],
 })
 export class ShareModule { }
