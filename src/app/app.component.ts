6eb679720752059fa9a2a602e673dbfa587f608f
lawliet529 <85656044+lawliet529@users.noreply.github.com>
2023-08-06 10:25:36 +0700

    Clean up

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,5 +1,4 @@
 import { Component } from '@angular/core';
-import { PRODUCT_DATA } from './data';
 
 @Component({
   selector: 'app-root',
@@ -8,37 +7,4 @@ import { PRODUCT_DATA } from './data';
 })
 export class AppComponent {
   title = 'finance';
-  products: Product[] = PRODUCT_DATA;
-
-    responsiveOptions: any[] | undefined;
-
-    // constructor(private productService: ProductService) {}
-
-    getSeverity(status: string): string {
-        switch (status) {
-            case 'INSTOCK':
-                return 'success';
-            case 'LOWSTOCK':
-                return 'warning';
-            case 'OUTOFSTOCK':
-                return 'danger';
-        }
-        return 'danger';
-    }
-
 }
-
-
-interface Product {
-  id?: string;
-  code?: string;
-  name?: string;
-  description?: string;
-  price?: number;
-  quantity?: number;
-  inventoryStatus?: string;
-  category?: string;
-  image?: string;
-  rating?: number;
-}
-
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,13 +2,8 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 
-
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-import { ImageModule } from 'primeng/image';
-import { CarouselModule } from 'primeng/carousel';
-import { TagModule } from 'primeng/tag';
-import { ButtonModule } from 'primeng/button';
 
 @NgModule({
   declarations: [
@@ -17,11 +12,7 @@ import { ButtonModule } from 'primeng/button';
   imports: [
     BrowserModule,
     BrowserAnimationsModule,
-    AppRoutingModule,
-    ImageModule,
-    CarouselModule,
-    TagModule,
-    ButtonModule
+    AppRoutingModule
   ],
   providers: [],
   bootstrap: [AppComponent]
